0ef0ca0317aa4d02534cacb5f455f1865c32ab50
Mahmoud Shata <83148301+MahmoudShata23@users.noreply.github.com>
2022-04-05 17:15:37 +0200

    first commit in task liked bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { DetailsComponent } from './product/details/details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductComponent,
+    DetailsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
